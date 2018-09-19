bd26251830c6bef34da3805972e0a02e1bb4f8a8
Andrew Ringler <public@andrewringler.com>
2018-09-19 11:06:30 -0400

    added working test code for Si4703 chip on LinkSprite board, for use with Arduino

---
+++ README.md
@@ -0,0 +1,2 @@
+Use Microbit with the Arduino IDE
+https://learn.adafruit.com/use-micro-bit-with-arduino/overview
---
+++ arduino/Si4703_Radio_Test/Si4703_Radio_Test.ino
@@ -0,0 +1,103 @@
+// Works with Arduino Mega and LinkSprite Si4703 FM Radio
+//
+// LinkSprite Si4703 FM Radio Board:
+// http://store.linksprite.com/breakout-board-for-si4703-fm-tuner/
+// http://linksprite.com/wiki/index.php?title=Main_Page
+// although the Si4703 chip is a 3.3V chip, the LinkSprite board needs to be 
+// powered at 3.3V, but expects 5V line levels for control
+// 
+// Getting Started:
+// 1) Install Si4703_FMRadio library, https://github.com/whiteneon/Si4703_FMRadio
+// 3) Wire up board pins: SDIO, SCLK, Reset, Ground, 3.3V
+// 2) Upload code to Arduino
+//
+#include <Si4703_Breakout.h>
+#include <Wire.h>
+
+int resetPin = 2;
+//int SDIO = A4;
+//int SCLK = A5;
+
+int SDIO = SDA; //SDA/A4 on Arduino
+int SCLK = SCL; //SCL/A5 on Arduino
+
+Si4703_Breakout radio(resetPin, SDIO, SCLK);
+int channel;
+int volume;
+char rdsBuffer[10];
+
+void setup()
+{
+  Serial.begin(57600);
+  Serial.println("\n\nSi4703_Breakout Test Sketch");
+  Serial.println("===========================");  
+  Serial.println("a b     Favourite stations");
+  Serial.println("+ -     Volume (max 15)");
+  Serial.println("u d     Seek up / down");
+  Serial.println("r       Listen for RDS Data (15 sec timeout)");
+  Serial.println("Send me a command letter.");
+  
+
+  radio.powerOn();
+//  radio.setVolume(0);
+
+  radio.setVolume(5);
+  radio.setChannel(881);
+}
+
+void loop()
+{
+  if (Serial.available())
+  {
+    char ch = Serial.read();
+    if (ch == 'u') 
+    {
+      channel = radio.seekUp();
+      displayInfo();
+    } 
+    else if (ch == 'd') 
+    {
+      channel = radio.seekDown();
+      displayInfo();
+    } 
+    else if (ch == '+') 
+    {
+      volume ++;
+      if (volume == 16) volume = 15;
+      radio.setVolume(volume);
+      displayInfo();
+    } 
+    else if (ch == '-') 
+    {
+      volume --;
+      if (volume < 0) volume = 0;
+      radio.setVolume(volume);
+      displayInfo();
+    } 
+    else if (ch == 'a')
+    {
+      channel = 930; // Rock FM
+      radio.setChannel(channel);
+      displayInfo();
+    }
+    else if (ch == 'b')
+    {
+      channel = 974; // BBC R4
+      radio.setChannel(channel);
+      displayInfo();
+    }
+    else if (ch == 'r')
+    {
+      Serial.println("RDS listening");
+      radio.readRDS(rdsBuffer, 15000);
+      Serial.print("RDS heard:");
+      Serial.println(rdsBuffer);      
+    }
+  }
+}
+
+void displayInfo()
+{
+   Serial.print("Channel:"); Serial.print(channel); 
+   Serial.print(" Volume:"); Serial.println(volume); 
+}
