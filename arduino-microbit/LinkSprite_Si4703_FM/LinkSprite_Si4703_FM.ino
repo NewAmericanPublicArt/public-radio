a659ceda635a1afcb94c1ad37d70928f02ed91e0
Andrew Ringler <public@andrewringler.com>
2018-09-19 12:39:28 -0400

    notes

--- arduino-microbit/LinkSprite_Si4703_FM/LinkSprite_Si4703_FM.ino
+++ arduino-microbit/LinkSprite_Si4703_FM/LinkSprite_Si4703_FM.ino
@@ -10,17 +10,18 @@
 // LinkSprite Si4703 FM Radio Board:
 // http://store.linksprite.com/breakout-board-for-si4703-fm-tuner/
 // http://linksprite.com/wiki/index.php?title=Main_Page
-// although the Si4703 chip is a 3.3V chip, the LinkSprite board needs to be 
+// although the Si4703 chip is a 3.3V chip, the LinkSprite board needs to be
 // powered at 3.3V, but expects 5V line levels for control
-// 
+// *** Hmmm, actually it seems the board works at 3.3V?
+//
 // Getting Started:
 // 1) Follow Microbit Arduino startup instructions: https://learn.adafruit.com/use-micro-bit-with-arduino/overview
 //    Install Si4703_FMRadio library, https://github.com/whiteneon/Si4703_FMRadio
 //    Install Adafruit GFX Library
 //    Install BLE Peripheral Library
 //    Install Adafruit_Microbit Library https://github.com/adafruit/Adafruit_Microbit/archive/master.zip
-//    
-//    
+//
+//
 // 3) Wire up board pins: SDIO, SCLK, Reset, Ground, 3.3V
 // 2) Upload code to Arduino
 //
@@ -41,80 +42,87 @@ int volume = 5;
 char rdsBuffer[10];
 Adafruit_Microbit_Matrix microbit;
 
+const int buttonA = 5;     // the number of the pushbutton pin
+const int buttonB = 11;     // the number of the pushbutton pin
+
 void setup()
 {
   Serial.begin(57600);
   Serial.println("\n\nSi4703_Breakout Test Sketch");
-  Serial.println("===========================");  
+  Serial.println("===========================");
   Serial.println("a b     Favourite stations");
   Serial.println("+ -     Volume (max 15)");
   Serial.println("u d     Seek up / down");
   Serial.println("r       Listen for RDS Data (15 sec timeout)");
   Serial.println("Send me a command letter.");
-  
-  microbit.begin();
+
+  pinMode(buttonA, INPUT);
+  pinMode(buttonB, INPUT);
+
+//  microbit.begin();
 
   radio.powerOn();
-//  radio.setVolume(0);
+  //  radio.setVolume(0);
 
   radio.setVolume(volume);
   radio.setChannel(channel);
 }
 
 void loop() {
-  microbit.print(channel);
-  
-  if (Serial.available())
-  {
+//  microbit.print(channel);
+
+  // Use Microbit buttons to seek up and down
+  if (! digitalRead(buttonA)) {
+    seekDown();
+  }
+  if (!digitalRead(buttonB)) {
+    seekUp();
+  }
+
+  if (Serial.available()) {
     char ch = Serial.read();
-    if (ch == 'u') 
-    {
-      channel = radio.seekUp();
-      displayInfo();
-    } 
-    else if (ch == 'd') 
-    {
-      channel = radio.seekDown();
-      displayInfo();
-    } 
-    else if (ch == '+') 
-    {
+    if (ch == 'u') {
+      seekUp();
+    } else if (ch == 'd') {
+      seekDown();
+    } else if (ch == '+') {
       volume ++;
       if (volume == 16) volume = 15;
       radio.setVolume(volume);
       displayInfo();
-    } 
-    else if (ch == '-') 
-    {
+    } else if (ch == '-') {
       volume --;
       if (volume < 0) volume = 0;
       radio.setVolume(volume);
       displayInfo();
-    } 
-    else if (ch == 'a')
-    {
+    } else if (ch == 'a') {
       channel = 930; // Rock FM
       radio.setChannel(channel);
       displayInfo();
-    }
-    else if (ch == 'b')
-    {
+    } else if (ch == 'b') {
       channel = 974; // BBC R4
       radio.setChannel(channel);
       displayInfo();
-    }
-    else if (ch == 'r')
-    {
+    } else if (ch == 'r') {
       Serial.println("RDS listening");
       radio.readRDS(rdsBuffer, 15000);
       Serial.print("RDS heard:");
-      Serial.println(rdsBuffer);      
+      Serial.println(rdsBuffer);
     }
   }
 }
 
-void displayInfo()
-{
-   Serial.print("Channel:"); Serial.print(channel); 
-   Serial.print(" Volume:"); Serial.println(volume); 
+void seekUp() {
+  channel = radio.seekUp();
+  displayInfo();
+}
+
+void seekDown() {
+  channel = radio.seekDown();
+  displayInfo();
+}
+
+void displayInfo() {
+  Serial.print("Channel:"); Serial.print(channel);
+  Serial.print(" Volume:"); Serial.println(volume);
 }
