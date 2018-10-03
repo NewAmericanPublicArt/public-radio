a199cd038a635d1bbadce35ec7f343db1a08fd27
Andrew Ringler <public@andrewringler.com>
2018-10-03 15:25:26 -0400

    track average speed over time of two photogates

--- arduino-microbit/Photogate_Blink/Photogate_Blink.ino
+++ arduino-microbit/Photogate_Blink/Photogate_Blink.ino
@@ -1,30 +1,62 @@
 #include <Adafruit_Microbit.h>
 Adafruit_Microbit_Matrix microbit;
 
+const int PHOTOGATE1_PIN = 1;
+const int PHOTOGATE2_PIN = 2;
+float speed1 = 0;
+float speed2 = 0;
+
 void setup() {
   microbit.begin();
 
-  pinMode(1, INPUT);
-  pinMode(2, INPUT);
+  pinMode(PHOTOGATE1_PIN, INPUT);
+  pinMode(PHOTOGATE2_PIN, INPUT);
 }
 
 void loop() {
   /* If pin1 is low we have obstructed the beam
       so flash a line of LEDs
   */
-  if (digitalRead(1) == LOW) {
+  if (digitalRead(PHOTOGATE1_PIN) == LOW) {
     microbit.drawLine(0, 0, 4, 0, LED_ON);
   } else {
     microbit.drawLine(0, 0, 4, 0, LED_OFF);
   }
 
-  /* If pin1 is low we have obstructed the beam
+  /* If pin2 is low we have obstructed the beam
     so flash a line of LEDs
   */
-  if (digitalRead(2) == LOW) {
+  if (digitalRead(PHOTOGATE2_PIN) == LOW) {
     microbit.drawLine(0, 1, 4, 1, LED_ON);
   } else {
     microbit.drawLine(0, 1, 4, 1, LED_OFF);
   }
 
+  measure();
+
+  // turn on a scale of LEDs that increases with increasing speed
+  int lastLEDIndex = 0;
+  if (speed1 == 0) {
+    microbit.drawLine(0, 2, 4, 2, LED_OFF);
+  } else {
+    lastLEDIndex = constrain(round(speed1 * 20), 0, 4);
+    if (lastLEDIndex + 1 < 4) {
+      microbit.drawLine(lastLEDIndex + 1, 2, 4, 2, LED_OFF);
+    }
+    if (lastLEDIndex >= 0) {
+      microbit.drawLine(0, 2, lastLEDIndex, 2, LED_ON);
+    }
+  }
+
+  if (speed2 == 0) {
+    microbit.drawLine(0, 3, 4, 3, LED_OFF);
+  } else {
+    lastLEDIndex = constrain(round(speed2 * 20), 0, 4);
+    if (lastLEDIndex + 1 < 4) {
+      microbit.drawLine(lastLEDIndex + 1, 3, 4, 3, LED_OFF);
+    }
+    if (lastLEDIndex >= 0) {
+      microbit.drawLine(0, 3, lastLEDIndex, 3, LED_ON);
+    }
+  }
 }
---
+++ arduino-microbit/Photogate_Blink/photogate.ino
@@ -0,0 +1,58 @@
+const int PHOTOGATE_LOG_BUFFER_SIZE = 101;
+const long READ_DELAY = 5;
+
+// sliding buffers for storing log data
+long lastRead = millis();
+int photogateLogIndex = 0;
+boolean photogateLog1[PHOTOGATE_LOG_BUFFER_SIZE];
+boolean photogateLog2[PHOTOGATE_LOG_BUFFER_SIZE];
+boolean bufferReady = false;
+float totalTransitions = ceil(((float)PHOTOGATE_LOG_BUFFER_SIZE) / 2.0);
+
+void measure() {
+  // Read our data
+  if (millis() - lastRead > READ_DELAY) {
+    lastRead = millis();
+    photogateLog1[photogateLogIndex] = digitalRead(PHOTOGATE1_PIN) == LOW;
+    photogateLog2[photogateLogIndex] = digitalRead(PHOTOGATE2_PIN) == LOW;
+
+    // let the buffer fill out before we use it
+    if (!bufferReady && photogateLogIndex == PHOTOGATE_LOG_BUFFER_SIZE - 1) {
+      bufferReady = true;
+    }
+
+    // update our speed data
+    // we will say that speed is number of transitions from HIGH to LOW
+    // out of total possible transitions
+    updateSpeedData();
+
+    // update index for next log
+    photogateLogIndex = (photogateLogIndex + 1) % PHOTOGATE_LOG_BUFFER_SIZE;
+  }
+}
+
+void updateSpeedData() {
+  if (bufferReady) {
+    int transitions = 0;
+    for (int i = 1; i < PHOTOGATE_LOG_BUFFER_SIZE; i++) {
+      if (photogateLog1[i] && !photogateLog1[i - 1]) {
+        transitions++;
+      }
+    }
+    if (photogateLog1[0] && !photogateLog1[PHOTOGATE_LOG_BUFFER_SIZE - 1]) {
+      transitions++;
+    }
+    speed1 = (float)transitions / totalTransitions;
+
+    transitions = 0;
+    for (int i = 1; i < PHOTOGATE_LOG_BUFFER_SIZE; i++) {
+      if (photogateLog2[i] && !photogateLog2[i - 1]) {
+        transitions++;
+      }
+    }
+    if (photogateLog2[0] && !photogateLog2[PHOTOGATE_LOG_BUFFER_SIZE - 1]) {
+      transitions++;
+    }
+    speed2 = (float)transitions / totalTransitions;
+  }
+}
