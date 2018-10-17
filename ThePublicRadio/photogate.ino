95b5ed9b1b1a17a9e2c70197490e9e5991608134
Andrew Ringler <public@andrewringler.com>
2018-10-17 17:10:41 -0400

    direction now working on channel wheel, maybe volume too, hard to tell

--- ThePublicRadio/photogate.ino
+++ ThePublicRadio/photogate.ino
@@ -20,6 +20,10 @@ long timeOfTransition2A = 0;
 long timeOfTransition2A_prev = 0;
 boolean photogate2B = false;
 boolean photogate2B_prev = false;
+int reading1_TwoBitGrayCode = 0; // combination of 1A and 1B
+int reading1_TwoBitGrayCode_prev = 0;
+int reading2_TwoBitGrayCode = 0; // combination of 2A and 2B
+int reading2_TwoBitGrayCode_prev = 0;
 
 int channelDirection = 1;
 int volumeDirection = 1;
@@ -45,9 +49,19 @@ void readPhotogatesForTuningAndVolume() {
     photogate2A = digitalRead(PHOTOGATE2A_PIN) == HIGH;
     photogate2B = digitalRead(PHOTOGATE2B_PIN) == HIGH;
 
+    // measure any edge of the slit for phase/direction
+    // see https://en.wikipedia.org/wiki/Rotary_encoder, https://commons.wikimedia.org/wiki/File:Incremental_directional_encoder.gif
+    // https://www.ethz.ch/content/dam/ethz/special-interest/mavt/dynamic-systems-n-control/idsc-dam/Lectures/Embedded-Control-Systems/LectureNotes/3_Position_and_Velocity_Measurements.pdf
+    // AKA a 2-bit Gray code pattern
+    reading1_TwoBitGrayCode_prev = reading1_TwoBitGrayCode;
+    reading2_TwoBitGrayCode_prev = reading2_TwoBitGrayCode;
+    reading1_TwoBitGrayCode = photogate1A * 2 + photogate1B;
+    reading2_TwoBitGrayCode = photogate2A * 2 + photogate2B;
+
     // update speed based on just the readings from the A sensors
     if (ready) {
-      // only measure one edge of the slit
+      // only measure one edge of the slit for speed
+      // so we have a reliably consistent speed
       speed1 = 0;
       speed2 = 0;
       if (photogate1A == HIGH && photogate1A_prev == LOW) {
@@ -59,36 +73,56 @@ void readPhotogatesForTuningAndVolume() {
         speed2 = 1; // min-speed
       }
 
-      channelUp(speed1); // update channel based on speed
-      volumeUp(speed2); // update volume based on speed
+      int reading1_Change = reading1_TwoBitGrayCode - reading1_TwoBitGrayCode_prev;
+      int reading2_Change = reading2_TwoBitGrayCode - reading2_TwoBitGrayCode_prev;
+      if (reading1_Change == 1) {
+        channelDirection = -1;
+      } else if (reading1_Change == -1) {
+        channelDirection = 1;
+      }
+      if (reading2_Change == 1) {
+        volumeDirection = -1;
+      } else if (reading2_Change == -1) {
+        volumeDirection = 1;
+      }
+
+      channelChange(speed1); // update channel based on speed
+      volumeChange(speed2); // update volume based on speed
     }
   }
 }
 
-void volumeUp(float speed) {
+void volumeChange(float speed) {
   if (speed > 0) {
     volume = volume + (speed * volumeDirection);
     if (volume > MAX_VOLUME) {
       volume = MAX_VOLUME;
-      volumeDirection *= -1; // change direction, AKA Dan's avacado volume
+      volumeDirection *= -1; // change direction, AKA Dan's avocado volume
     }
     if (volume < MIN_VOLUME) {
       volume = MIN_VOLUME;
-      volumeDirection *= -1; // change direction, AKA Dan's avacado volume
+      volumeDirection *= -1; // change direction, AKA Dan's avocado volume
     }
     radio.setVolume(int(round(volume)));
   }
 }
 
-void channelUp(float speed) {
+void channelChange(float speed) {
   if (speed > 0) {
-    channel = int(channel + 2 * speed);
+    // multiply by 2, U.S. stations are only on odd station numbers
+    channel = int(channel + 2 * speed * channelDirection);
+
+    // always make sure we have an odd channel
     if (channel % 2 != 1) {
-      // make sure we have an odd channel
-      channel++;
+      channel = channel + 1 * channelDirection;
     }
+
+    // loop around channel
     if (channel > MAXFREQ) {
-      channel = MINFREQ; // loop around
+      channel = MINFREQ;
+    }
+    if (channel < MINFREQ) {
+      channel = MAXFREQ;
     }
     radio.setChannel(channel);
   }
